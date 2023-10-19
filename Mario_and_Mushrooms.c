a211f1f2284536e2d05ac6ac655eb191e0bbdf40
maneendra2006 <147416105+maneendra2006@users.noreply.github.com>
2023-10-19 15:12:15 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d",&a);     b=0;     b=(b+a)%3;     if(b==2)     {         printf("SMALL");     }     else if(b==1)     {         printf("HUGE");     }     else if(b==0)     {         printf("NORMAL");     }
    }

---
+++ Mario_and_Mushrooms.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    b=0;
+    b=(b+a)%3;
+    if(b==2)
+    {
+        printf("SMALL");
+    }
+    else if(b==1)
+    {
+        printf("HUGE");
+    }
+    else if(b==0)
+    {
+        printf("NORMAL");
+    }
+    
+}
