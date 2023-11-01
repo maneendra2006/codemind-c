81e1d80e9da35d581115d5511f02eb771a45086c
maneendra2006 <147416105+maneendra2006@users.noreply.github.com>
2023-11-01 11:00:00 +0530

    #include<stdio.h> int main() {     int a,b,c,i;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {
            c=a*i;
            if(c%b==0)
            {
                printf("%d",a*b/c);
                break;
            }
        }
    }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,i;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+    	
+        c=a*i;
+        if(c%b==0)
+        {
+            printf("%d",a*b/c);
+            break;
+        }
+    }
+}
