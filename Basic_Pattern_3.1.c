6f1ac3a70bb2ceda4bacb3a8c3ba732a180a3e6e
maneendra2006 <147416105+maneendra2006@users.noreply.github.com>
2023-11-07 20:06:53 +0530

    #include<stdio.h> int main() { 	int a,i,j,k; 	scanf("%d",&a); 	for(i=a;i>=1;i--) 	{ 		for(j=1;j<=i;j++) 		{
           	   printf("* ");
    	   }
    	   printf("
    ");
    
    }
         
    	
    }

---
+++ Basic_Pattern_3.1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a,i,j,k;
+	scanf("%d",&a);
+	for(i=a;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+		   	
+		
+       
+       	   printf("* ");
+	   }
+	   printf("
+");
+
+}
+     
+	
+}
